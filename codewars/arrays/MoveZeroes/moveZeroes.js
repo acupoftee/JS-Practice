89eda27df990fa4ef4af3e5a4bc39b70e223d4f6
acupoftee <teedia31@gmail.com>
2019-07-11 01:17:47 -0400

    moved all solutions to moveZeroes.js

--- codewars/arrays/MoveZeroes/moveZeroes.js
+++ codewars/arrays/MoveZeroes/moveZeroes.js
@@ -1,13 +1,26 @@
-var moveZeros = function (arr) {
-    // TODO: Program me
+const moveZeros = arr => {
     // 1. filter out zeroes
-    // 2. count how many zeroes we took out
-    // 3. for each zero filtered, push them to the end
     const noZeroes = arr.filter(x => x !== 0)
-    let zeroCount = arr.length - noZeroes.length
+
+    // 2. count how many zeroes we took out
+    const zeroCount = arr.length - noZeroes.length
+
+    // 3. for each zero filtered, push them to the end
     while (zeroCount > 0) {
       noZeroes.push(0)
       zeroCount--
     }
+
     return noZeroes
   }
+
+const moveZeroesTwo = arr => {
+  // 1. Filter out zeroes
+  const noZeroes = arr.filter(x => x !== 0)
+
+  // 2. Get the number of zeroes to add
+  const zeroCount = arr.length - noZeroes.length
+    
+  // 3. Add a new array filled with zeroes to the filtered array
+  return noZeroes.concat(Array(zeroCount).fill(0))
+}
--- codewars/arrays/MoveZeroes/moveZeroes2.js
+++
@@ -1,6 +0,0 @@
-const moveZeroes = arr => {
-    let noZeroes = arr.filter(x => x !== 0)
-    let zeroCount = arr.length - noZeroes.length
-    
-    return noZeroes.concat(new Array(zeroCount).fill(0))
-}
