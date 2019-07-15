7aa3f28d6e64905ab6092c4636bb3da1677f031c
acupoftee <teedia31@gmail.com>
2019-07-15 09:41:01 -0400

    Adding second solution for defangIpAddress

--- leetcode/strings/DefangingAnIPAddress/defangingIP.js
+++ leetcode/strings/DefangingAnIPAddress/defangingIP.js
@@ -5,3 +5,11 @@
 const defangIPaddr = address => {
     return address.split('').map(c => c === '.' ? '[.]' : c).join('')
 }
+
+/**
+ * @param {string} address the starter IP address 
+ * @return {string} The new IP address
+ */
+const defangIPaddrTwo = address => {
+    return address.split('.').join('[.]')
+}
